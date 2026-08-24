643d415f42793e3deb3460ad3b1191876000f63b
Matteo Ientile <matteoientile@gmail.com>
2026-08-24 16:40:55 +0200

    Adjusted default columns

--- app.py
+++ app.py
@@ -370,7 +370,8 @@ with tab1:
         'Nome', 'Squadra', 'Ruolo', 'Target_cr', 'Max_cr',
         'Presenze_Last_Season', 'Presenze_Tot',
         'FM_W', 'FM_Raw', 'MV_W', 'MV_Raw',
-        'P_ge_6', 'P_ge_6_5', 'P_lt_6', 'Tot_Gol', 'Tot_Ass'
+        'P_ge_6', 'P_ge_6_5', 'P_lt_6', 
+        'Bonus_per_Game', 'Malus_per_Game', 'Clean_Sheet_Rate'
     ]
 
     selected_columns = st.multiselect(
